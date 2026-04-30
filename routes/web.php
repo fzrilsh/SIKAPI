fe9b8ae8a6dbf04544e44478063fbf923df5f75f
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2026-04-30 20:43:23 +0700

    feat: refactor authentication routes to use controller methods and improve code organization

--- routes/web.php
+++ routes/web.php
@@ -1,24 +1,27 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PolicyController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('pages.home');
+Route::prefix('auth')->group(function () {
+    Route::get('/', [AuthController::class, 'index'])->name('login');
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::post('/register', [AuthController::class, 'register'])->name('register.post');
+    Route::post('/login', [AuthController::class, 'login'])->name('login.post');
 });
 
-Route::get('/kebijakan', function () {
-    return view('pages.kebijakan');
+Route::middleware('auth')->group(function () {
+    Route::get('/', HomeController::class)->name('home');
+
+    Route::get('/kebijakan', [PolicyController::class, 'index'])->name('policies.index');
+    Route::get('/kebijakan/{policy:slug}', [PolicyController::class, 'show'])->name('policies.show');
+
+    Route::post('/kebijakan/{policy}/comment', [PolicyController::class, 'storeComment'])->name('policies.comment');
+    Route::post('/comments/{comment}/interact', [PolicyController::class, 'interactComment'])
+        ->name('comments.interact');
+
+    Route::post('/kebijakan/{policy}/interact', [PolicyController::class, 'interact'])
+        ->name('policies.interact');
 });
-
-Route::get('/kebijakan/ruu-kesehatan', function () {
-    return view('pages.detail-kebijakan');
-});
-
-
-Route::get('/register', function () {
-    return view('pages.auth');
-})->name('register');
-
-Route::get('/login', function () {
-    return view('pages.auth');
-})->name('login');
