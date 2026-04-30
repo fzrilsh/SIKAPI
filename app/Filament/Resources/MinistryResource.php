b9d6c8d7d08230c988ba779c4d80dd36165f281d
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2026-04-30 20:40:44 +0700

    feat: add MinistryResource with form and table configuration for managing ministries

---
+++ app/Filament/Resources/MinistryResource.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\MinistryResource\Pages;
+use App\Filament\Resources\MinistryResource\RelationManagers;
+use App\Models\Ministry;
+use Filament\Forms;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+
+class MinistryResource extends Resource
+{
+    protected static ?string $model = Ministry::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-building-office-2';
+
+    protected static ?string $navigationGroup = 'User Management';
+
+    public static function shouldRegisterNavigation(): bool
+    {
+        return Auth::user()->is_super_admin;
+    }
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()->schema([
+                    TextInput::make('name')
+                        ->required()
+                        ->live(onBlur: true)
+                        ->afterStateUpdated(fn($state, callable $set) => $set('slug', Str::slug($state))),
+                    TextInput::make('slug')
+                        ->required()
+                        ->unique(Ministry::class, 'slug', ignoreRecord: true),
+                    FileUpload::make('logo')
+                        ->label('Logo Kementerian')
+                        ->image()
+                        ->avatar()
+                        ->imageEditor()
+                        ->directory('ministry-logos')
+                        ->alignLeft()
+                        ->columnSpanFull(),
+                ])->columns(2)
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name')->searchable()->sortable(),
+                TextColumn::make('slug'),
+                ImageColumn::make('logo')
+                    ->label('Logo')
+                    ->circular(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMinistries::route('/'),
+            'create' => Pages\CreateMinistry::route('/create'),
+            'edit' => Pages\EditMinistry::route('/{record}/edit'),
+        ];
+    }
+}
