a32f5833dd30f45e19243c849c57883ad49d7271
Fazril Syaveral Hillaby <fazriloke18@gmail.com>
2026-04-30 20:46:54 +0700

    feat: refactor authentication routes for improved organization and clarity

--- routes/web.php
+++ routes/web.php
@@ -5,23 +5,21 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PolicyController;
 use Illuminate\Support\Facades\Route;
 
-Route::prefix('auth')->group(function () {
-    Route::get('/', [AuthController::class, 'index'])->name('login');
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-    Route::post('/register', [AuthController::class, 'register'])->name('register.post');
-    Route::post('/login', [AuthController::class, 'login'])->name('login.post');
+Route::middleware('guest')->prefix('auth')->controller(AuthController::class)->group(function () {
+    Route::get('/', 'index')->name('login');
+    Route::post('/register', 'register')->name('register.post');
+    Route::post('/login', 'login')->name('login.post');
 });
 
 Route::middleware('auth')->group(function () {
     Route::get('/', HomeController::class)->name('home');
+    Route::post('/auth/logout', [AuthController::class, 'logout'])->name('logout');
 
-    Route::get('/kebijakan', [PolicyController::class, 'index'])->name('policies.index');
-    Route::get('/kebijakan/{policy:slug}', [PolicyController::class, 'show'])->name('policies.show');
-
-    Route::post('/kebijakan/{policy}/comment', [PolicyController::class, 'storeComment'])->name('policies.comment');
-    Route::post('/comments/{comment}/interact', [PolicyController::class, 'interactComment'])
-        ->name('comments.interact');
-
-    Route::post('/kebijakan/{policy}/interact', [PolicyController::class, 'interact'])
-        ->name('policies.interact');
+    Route::controller(PolicyController::class)->group(function () {
+        Route::get('/kebijakan', 'index')->name('policies.index');
+        Route::get('/kebijakan/{policy:slug}', 'show')->name('policies.show');
+        Route::post('/kebijakan/{policy}/interact', 'interact')->name('policies.interact');
+        Route::post('/kebijakan/{policy}/comment', 'storeComment')->name('policies.comment');
+        Route::post('/comments/{comment}/interact', 'interactComment')->name('comments.interact');
+    });
 });
